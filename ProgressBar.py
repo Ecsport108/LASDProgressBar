f5d0ad379ab3a91158b538879f8710b821852eee
Ecsport108 <ecsport108@yahoo.com>
2020-08-03 15:46:50 -0500

    Add files via upload

---
+++ ProgressBar.py
@@ -0,0 +1,15 @@
+import fnmatch, os, tkinter, tqdm
+from tkinter import filedialog
+from tqdm import tqdm
+root = tkinter.Tk()
+root.withdraw()
+print ("Please select the directory containing the LAS files you are using for the dataset.")
+workingpath = filedialog.askdirectory()
+las_count = len(fnmatch.filter(os.listdir(workingpath), '*.las'))
+lasx_count = 0
+pbar = tqdm(total = las_count)
+while lasx_count != las_count:
+	lasx_count = len(fnmatch.filter(os.listdir(workingpath), '*.lasx'))
+	pbar.n = lasx_count
+	pbar.update(0)
+print ("Script Complete.")
